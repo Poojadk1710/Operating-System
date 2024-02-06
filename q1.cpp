872d5f165ea42289579caa1a52a29161ea83d458
Pooja1710 <65893335+Poojadk1710@users.noreply.github.com>
2024-02-06 16:37:47 -0700

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,20 +1,21 @@
-#include <stdlib.h> // exit
-#include <sys/wait.h>
-#include <unistd.h> // fork
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
 
-int main() {
-  int x = 100;
-  int rc = fork();
-  if (rc < 0) {
-    fprintf(stderr, "fork failed\n");    // fork failed; exit
-    exit(EXIT_FAILURE);
-  } else if (rc == 0) {
-    x = 101;
-    printf("x in child process: %d\n", x);
-  } else {
-    x = 102;
-    printf("x in parent process: %d\n", x);
-    wait(NULL);
-  }
-  return 0;
+int main(){
+    int x = 10;
+    x = 100;
+    int rc = fork();
+    if(rc < 0)
+        printf("failed to fork\n");
+    else if(rc == 0){
+        x = 99;
+        printf("child process get x: %d\n", x);
+        exit(0);
+    }else{
+        x = 999;
+        printf("parent process set x to: %d\n", x);
+    }
+
+    return 0;
 }
