18b6be084f11c263751c7a5222cbea36d5224e76
Pooja1710 <65893335+Poojadk1710@users.noreply.github.com>
2024-02-07 04:03:35 -0700

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,27 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 
-int main(void){
+int main()
+{
     int fd, rc;
-    fd = open("output.txt", O_WRONLY | O_APPEND);
-    if (fd < 0) {
-        perror("open");
+    fd = open("Output.txt", O_WRONLY | O_APPEND);
+    
+    if (fd < 0) 
+    {
+     
+        perror("Open");
         exit(EXIT_FAILURE);
+        
     }
 
-    char buf1[] = "content1\n";
-    char buf2[] = "content2\n";
+    char buf1[] = "File 1\n";
+    char buf2[] = "File 2\n";
     
     rc = fork();
-    if(rc < 0) {
-        printf("failed to fork\n");
-    } else if(rc == 0) {
-        //Child process
-        write(fd, buf1, sizeof(buf1) - 1); //remove '\0'
+    if(rc < 0)            //Failed fork
+    {
+        
+        printf("Failed to fork\n");
+    
+    } else if(rc == 0)     //Child process
+    {
+        const char * child_msg = "Hey, This is child process\n";
+        printf("The child process is writing on the file");
+        write(fd, buf1, sizeof(buf1) - 1);     //removes '\0'
         exit(0);
+        
     } else {
         //Parent process
         wait(NULL); // Wait for the child process to finish
