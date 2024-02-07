be20118c3074ec76dcfb362269ef3f6d2600a396
Pooja1710 <65893335+Poojadk1710@users.noreply.github.com>
2024-02-07 03:52:43 -0700

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,20 +1,36 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(){
-    int x = 10;
+    
+    int x;
     x = 100;
     int rc = fork();
-    if(rc < 0)
-        printf("failed to fork\n");
-    else if(rc == 0){
+    
+    if(rc < 0)                    //Error
+    {
+        
+        fprintf(stderr, "Failed to fork\n"); 
+        
+    }
+    else if(rc == 0){             //Child process
+
+        printf("Child process\n");
+        printf("The Current value of X: %d\n", x);
         x = 99;
-        printf("child process get x: %d\n", x);
-        exit(0);
-    }else{
-        x = 999;
-        printf("parent process set x to: %d\n", x);
+        printf("Child process will now get x as : %d\n", x);
+        
+        
+    }else{                        //Parent process
+
+        wait(NULL);
+        printf("Parent process\n");
+        printf("The Current value of X: %d\n", x);
+        x = 101;
+        printf("Parent process will now be set x to : %d\n", x);
+    
     }
 
     return 0;
