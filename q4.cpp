80b722d0fbe769bbb3a0d66af71a45a003998e6c
Pooja1710 <65893335+Poojadk1710@users.noreply.github.com>
2024-02-07 05:36:45 -0700

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -5,21 +5,26 @@
 #include <unistd.h>   // fork
 #include <signal.h>
 
-int main(void){
+int main()
+{    
     int rc, status, pid;
     rc = fork();
 
-    if(rc < 0)
-        printf("failed to fork\n");
-    else if(rc == 0){
-        // int p = wait(NULL);
-        printf("Hello from child (%d), p = %d\n", getpid(), p);
+    if(rc < 0)        //Failed fork
+    {
+        fprintf(stderr, "Failed to fork\n");
+        
+    } else if(rc == 0){    //Child process
+        
+        int p = wait(NULL);
+        printf("Hello from child (%d), p = %d\n", p);
         exit(0);
     }
-    else{
+    else{                    //Parent process
         pid = wait(&status); 
-        if(WIFEXITED(status))
-            printf("normal termination of child (%d), exit status = %d\n", pid, WEXITSTATUS(status));
+        if(WIFEXITED(status)){
+            printf("Normal termination of child (%d), exit status = %d\n", pid, WEXITSTATUS(status));
+        }
         printf("Goodbye from parent (%d)\n", getpid());
     }
 }
