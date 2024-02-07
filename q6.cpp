150a8a4b411dcff505c42520d18e677b9e68c024
Pooja1710 <65893335+Poojadk1710@users.noreply.github.com>
2024-02-07 06:06:34 -0700

    Update q6.cpp

--- q6.cpp
+++ q6.cpp
@@ -3,21 +3,29 @@
 #include <stdlib.h>   // exit
 #include <sys/wait.h> // wait
 #include <unistd.h>   // fork
-#include <signal.h>
 
 
-int main(){
+int main()
+{
     int rc, status, pid;
     rc = fork();
 
-    if(rc < 0)
-        printf("failed to fork\n");
-    else if(rc == 0){
+    if(rc < 0)        //Failed fork
+    {
+        
+        printf("Failed to fork\n");
+    
+    }else if(rc == 0)   //Child process
+    {
+        
         close(STDOUT_FILENO);
-        printf("child (%d) print to stdandard output.\n", getpid());
+        printf("Child (%d) print to stdandard output.\n", getpid());
         exit(0);
+        
     }
-    else{
+    else{                //Parent process
+        
         printf("Goodbye from parent (%d)\n", getpid());
+    
     }
 }
